6dd8ddee44c8d4b246c0d624d933dd2b9af561e7
Kiselev_M <backupwow2085@gmail.com>
2026-05-05 07:34:20 +0300

    выполнено 7.3

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,30 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
-  });
+	it("book titles are equal", () => {
+		expect(
+			sorting.sortByName([
+				"Гарри Поттер",
+				"Властелин Колец",
+				"Властелин Колец",
+			])
+		).toEqual([
+			"Властелин Колец",
+			"Властелин Колец",
+			"Гарри Поттер",
+		]);
+	});
+	it("Books names should be sorted in ascending order", () => {
+		expect(
+			sorting.sortByName([
+				"Гарри Поттер",
+				"Властелин Колец",
+				"Волшебник изумрудного города",
+			])
+		).toEqual([
+			"Властелин Колец",
+			"Волшебник изумрудного города",
+			"Гарри Поттер",
+		]);
+	});
 });
